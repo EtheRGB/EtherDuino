43a8960367f794e9551241ea16b2025f783451d9
inselc <mrbsod.ok@googlemail.com>
2017-04-17 13:40:11 +0200

    Improved debug messages
    Debug messages will now show the file name and line number.

--- core/Debug/debug.h
+++ core/Debug/debug.h
@@ -5,14 +5,21 @@
  *  Author: inselc
  */ 
 
-
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
 #include "../../core/Serial/Serial.h"
+#include <stdlib.h>
 
-#define DEBUG_LOG(x) serialWriteStr("\r\n -- DEBUG . "); \
-					 serialWriteStr(x); \
-					 serialWriteStr(" --\r\n")
+static inline void ERROR_LOG(const char* x) 
+{
+	serialWriteStr("\r\n Error in " __FILE__ ":");
+	char n[4];
+	itoa(__LINE__, n, 10);
+	serialWriteBuf((uint8_t*)n, 4, -1);
+	serialWriteStr(" ");
+	serialWriteStr(x);
+	serialWriteStr("\r\n");
+}
 
 #endif /* DEBUG_H_ */
