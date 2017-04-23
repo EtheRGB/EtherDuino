de186f01bde88e47c76adceb1b53266027777ebf
inselc <mrbsod.ok@googlemail.com>
2017-04-23 16:50:15 +0200

    Extends Ethernet implementation
    Modified existing methods from e20862e0f9063b7489c0e6741e9ec33f7028ad55
    to use int type instead of uint16_t.
    Added UDP read and write functions.
    Added TCP client mode

--- core/Ethernet/Ethernet.h
+++ core/Ethernet/Ethernet.h
@@ -1,31 +1,41 @@
-/*
- * Ethernet.h
+/*! @brief Data exchange over Ethernet
  *
- * Created: 12/11/2016 3:21:41 AM
- *  Author: inselc
- */ 
+ *	@author inselc
+ *	@date 13.12.16			Initial version
+ *	@date 10.04.17			Added socket ops, tcp r/w				
+ *	@date 18.04.17			Added udp, ipraw r/w
+ *	@date 23.04.17			Changed uint16 to bool/int
+ *	@date 23.04.17			Added peer_t type						*/
 
 #ifndef ETHERNET_H_
 #define ETHERNET_H_
 
+/*! @file */
+
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef uint8_t socket_t;
 
-void ethInit(void);
+typedef struct {
+	uint8_t ip[4];
+	uint16_t port;
+} peer_t;
+
+void ethInit(uint8_t txMemSizes, uint8_t rxMemSizes);
 void ethSetLocalIP(uint8_t mac[6], uint8_t subnet[4], uint8_t ip[4]);
 
-uint8_t ethSockOpen(socket_t socket, uint16_t port, uint8_t protocol, uint8_t modeFlags);
+bool ethSockOpen(socket_t socket, uint16_t port, uint8_t protocol, uint8_t modeFlags);
 void ethSockClose(socket_t socket);
 
-uint8_t ethSockListen(socket_t socket);
-uint8_t ethSockConnect(socket_t socket, uint8_t host, uint8_t port);
+bool ethSockListen(socket_t socket);
+bool ethSockConnect(socket_t socket, peer_t* targetPeer);
 void ethSockDisconnect(socket_t socket);
 
-uint16_t ethAvailable(socket_t socket);
-uint16_t ethRead(socket_t socket, uint8_t* dataBuffer, uint16_t bufSize);
-uint16_t ethReadFrom(socket_t socket, uint8_t srcIP[4], uint16_t port, uint8_t* dataBuffer, uint16_t bufSize);
-uint16_t ethWrite(socket_t socket, uint8_t* dataBuffer, uint16_t dataLength);
-uint16_t ethWriteTo(socket_t socket, uint8_t destIP[4], uint16_t port, uint8_t* dataBuffer, uint16_t dataLength);
+int ethAvailable(socket_t socket);
+int ethRead(socket_t socket, uint8_t* dataBuffer, int bufSize);
+int ethReadFrom(socket_t socket, peer_t* sourcePeer, uint8_t* dataBuffer, int bufSize);
+int ethWrite(socket_t socket, uint8_t* dataBuffer, int dataLength);
+int ethWriteTo(socket_t socket, peer_t* targetPeer, uint8_t* dataBuffer, int dataLength);
 
 #endif /* ETHERNET_H_ */
