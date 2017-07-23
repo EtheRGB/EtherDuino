0decaa29ff85875cae746e358c1d9a84130bf705
inselc <mrbsod.ok@googlemail.com>
2017-07-24 01:01:17 +0200

    Adds array helpers

---
+++ tools/ArrayTools.h
@@ -0,0 +1,13 @@
+/*!	@brief Array helper macros
+ *
+ *	@author	inselc
+ *	@date	08.07.17		First implementation					*/
+
+#ifndef ARRAYTOOLS_H_
+#define ARRAYTOOLS_H_
+
+/*!	@file */
+
+#define LENGTHOF(x) (sizeof((x)) / sizeof(*(x)))
+
+#endif /* ARRAYTOOLS_H_ */
